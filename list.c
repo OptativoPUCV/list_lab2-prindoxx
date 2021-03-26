8567a4a7c6812b7ad5a40a2a184d3b9a73cb633b
Replit user <replituser@example.com>
2021-03-26 19:53:54 +0000

    new try

--- list.c
+++ list.c
@@ -66,6 +66,10 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) {
+  if (list->tail && list->tail->data){
+    list->current = list->tail;
+    return (void*) list->current->data;
+  }
     return NULL;
 }
 
